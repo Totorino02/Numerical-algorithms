1f344c8624fffd894e98e0d594a4a019f16560d4
totorino <antoinehounsi3@gmail.com>
2022-02-05 00:09:47 +0000

    gitignore

--- linearEq/main.py
+++ linearEq/main.py
@@ -2,7 +2,7 @@ from gaussJordan import gaussJordan
 from gauss import gauss
 
 # test of gaus-Jordan method
-alGaussJ = gauss("/home/totorino/Bureau/CIC Semestre3/MTH300/mth 300 algo numérique/codes/eqLineaire/linearEq/matrix.txt")
-alGaussJ.triangularize()
-alGaussJ.showTM()
+alGaussJ = gaussJordan("/home/totorino/Bureau/CIC Semestre3/MTH300/mth 300 algo numérique/codes/eqLineaire/linearEq/matrix.txt")
+#alGaussJ.triangularize()
+#alGaussJ.showTM()
 alGaussJ.showResult()
