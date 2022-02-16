8db88905a51f9c96aac8c27ff77dbd71a4510c49
totorino <antoinehounsi3@gmail.com>
2022-02-16 21:39:20 +0000

    choleski 1.1.0

--- linearEq/choleski.py
+++ linearEq/choleski.py
@@ -4,7 +4,6 @@
     Author: HOUNSI Madouvi antoine-sebastien
     Date: 16/02/2022
 """
-import math
 import sys
 from math import pow
 
---
+++ linearEq/determinant.py
@@ -0,0 +1,6 @@
+"""
+    Name: choleski.py
+    Goal: Numerical resolution of linear equations with the method of choleski
+    Author: HOUNSI Madouvi antoine-sebastien
+    Date: 16/02/2022
+"""
